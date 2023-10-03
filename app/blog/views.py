b26cb66270d7c733b91cfbd655c60eb19033a217
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-10-03 00:54:05 -0300

    Implementar postagens recentes

--- app/blog/views.py
+++ app/blog/views.py
@@ -3,18 +3,19 @@ from django.shortcuts import render
 from .models import Categoria, Postagem
 
 MAX_PAGINAS_DESTACADAS = 2
+MAX_PAGINAS_RECENTES = 5
 
 
 def pagina_inicial(request):
     categorias = Categoria.objects.all()
     postagens = Postagem.objects.all()
-    postagens_destacadas_recente = postagens.filter(destaque=True)[
-        :MAX_PAGINAS_DESTACADAS
-    ]
+    postagens_destacadas = postagens.filter(destaque=True)[:MAX_PAGINAS_DESTACADAS]
+    postagens_recentes = postagens.order_by("-data_publicacao")[:MAX_PAGINAS_RECENTES]
 
     context = {
         "categorias": categorias,
         "postagens": postagens,
-        "postagens_destacadas": postagens_destacadas_recente,
+        "postagens_destacadas": postagens_destacadas,
+        "postagens_recentes": postagens_recentes,
     }
     return render(request, "pagina_inicial.html", context)
