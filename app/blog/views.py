6f14d8de401c9c6a75f5530cddfd8191e12c2007
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-10-03 11:45:04 -0300

    Adicionar filtragem de postagens por data

--- app/blog/views.py
+++ app/blog/views.py
@@ -11,11 +11,21 @@ def pagina_inicial(request):
     postagens = Postagem.objects.all()
     postagens_destacadas = postagens.filter(destaque=True)[:MAX_PAGINAS_DESTACADAS]
     postagens_recentes = postagens.order_by("-data_publicacao")[:MAX_PAGINAS_RECENTES]
+    data_postagens_recentes = postagens.dates("data_publicacao", "month", order="DESC")
 
     context = {
         "categorias": categorias,
         "postagens": postagens,
         "postagens_destacadas": postagens_destacadas,
         "postagens_recentes": postagens_recentes,
+        "data_postagens_recentes": data_postagens_recentes,
     }
     return render(request, "pagina_inicial.html", context)
+
+
+def postagens_por_data(request, mes, ano):
+    postagens = Postagem.objects.filter(
+        data_publicacao__month=mes, data_publicacao__year=ano
+    )
+    context = {"postagens": postagens}
+    return render(request, "postagens_por_data.html", context)
