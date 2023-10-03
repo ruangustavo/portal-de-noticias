ed79d12bba2282d319d409acccc39d809348190b
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-10-03 15:47:22 -0300

    Adicionar página de detalhar postagem

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.pagina_inicial, name="pagina_inicial"),
     path(
+        "postagens/<int:postagem_id>/",
+        views.detalhar_postagem,
+        name="detalhar_postagem",
+    ),
+    path(
         "postagens/<int:ano>/<int:mes>/",
         views.postagens_por_data,
         name="postagens_por_data",
